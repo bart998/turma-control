63311e7ddcdc3e046550e89da644eb376801cefd
joao <joao@DESKTOP-ETRUNCC.localdomain>
2026-05-08 16:11:16 -0300

    criando funcionalidade de notas

---
+++ cadastro_de_notas.ts
@@ -0,0 +1,19 @@
+const tela = require("readline-sync")
+const { titulo } = require("./utils.ts")
+
+let notas:string[] = []
+
+function telaCadastrarNotas() {
+   titulo("CADASTRAR DE NOTAS")
+ss<F12>
+   const n1 = tela.question("informe a primeira nota: ")
+   const n2 = tela.question("informe a segunda nota: ")
+   const n3 = tela.question("informe a terceira nota: ")
+
+
+  notas.push(`${n1};${n2},${3}`)
+
+}
+
+export = { telaCadastrarNotas }
+
